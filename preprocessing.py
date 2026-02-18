15f8d83fc8bfe9f4d28d50d578e5afecc1b2da96
Donghan Kim <donghan.kim02@gmail.com>
2026-02-18 17:01:37 +0100

    preprocessing check

--- preprocessing.py
+++ preprocessing.py
@@ -1,51 +1,42 @@
+import numpy as np
 import mne
 from mne.datasets import eegbci
 from mne.io import read_raw_edf, concatenate_raws
-from mne.channels import make_standard_montage
-from mne import Epochs, pick_types
+from mne import events_from_annotations, Epochs, pick_types
 
-subject = 1
-runs = [4, 8, 12]
-tmin, tmax = -1.0, 4.0
+def get_data(subject, runs):
+    raw_fnames = eegbci.load_data(subject, runs, verbose=False)
+    raw_list = [read_raw_edf(f, preload=True, verbose=False) for f in raw_fnames]
+    raw = concatenate_raws(raw_list)
+    
+    eegbci.standardize(raw)
+    montage = mne.channels.make_standard_montage('standard_1020')
+    raw.set_montage(montage)
 
-print("Loading data from PhysioNet...")
-raw_fnames = eegbci.load_data(subject, runs)
-raw_list = [read_raw_edf(f, preload=True) for f in raw_fnames]
-raw = concatenate_raws(raw_list)
+    raw.filter(7., 30., fir_design='firwin', skip_by_annotation='edge', verbose=False)
+    
+    event_id = dict(T1=0, T2=1) 
+    events, _ = events_from_annotations(raw, event_id=event_id, verbose=False)
 
-print("Standardizing data...")
-eegbci.standardize(raw)
-montage = make_standard_montage('standard_1020')
-raw.set_montage(montage)
+    picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
+    
+    tmin, tmax = -1.0, 4.0 
+    epochs = Epochs(
+        raw, 
+        events, 
+        event_id, 
+        tmin, tmax, 
+        proj=True, 
+        picks=picks, 
+        baseline=None, 
+        preload=True,
+        verbose=False
+    )
+    
 
-print("Renaming annotations...")
-raw.annotations.rename(dict(T1='hands', T2='feet'))
-
-print("Setting EEG reference... Setting average reference")
-raw.set_eeg_reference(projection=True)
-
-print("Filtering data...")
-raw.filter(7.0, 30.0, fir_design="firwin", skip_by_annotation="edge")
-
-print("Extracting epochs...")
-picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
-
-epochs = Epochs(
-    raw,
-    event_id=["hands", "feet"],
-    tmin=tmin,
-    tmax=tmax,
-    proj=True,
-    picks=picks,
-    baseline=None,
-    preload=True,
-)
-
-epochs_train = epochs.copy().crop(tmin=1.0, tmax=2.0)
-
-X = epochs_train.get_data()
-y = epochs.events[:, -1] - 2
-
-print("X shape:", X.shape)
-print("y shape:", y.shape)
-print("label values:", set(y))
+    epochs_train = epochs.copy().crop(tmin=1.0, tmax=2.0)
+    
+    X = epochs_train.get_data() 
+    y = epochs_train.events[:, -1] 
+    
+    return X, y
