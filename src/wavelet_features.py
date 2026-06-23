8f496fdb35a332834f8d448d09fe5e11969086f8
Donghan Kim <donghan.kim02@gmail.com>
2026-06-23 23:50:11 +0200

    feature: added mother morlet wavelet function

--- src/wavelet_features.py
+++ src/wavelet_features.py
@@ -1,4 +1,17 @@
-# Place holder for wavelet feature extraction code.
+import numpy as np
 
-def morlet_mother_wavelet(t, f0=1.0, sigma=1.0):
-    pass
+def mother_morlet(
+        eta: np.ndarray,
+        w0: float = 6.0,
+        corrected: bool = True
+) -> np.ndarray:
+    normalization = np.pi ** (-0.25)
+    carrier = np.exp(1j * w0 * eta)    # Carrier wave
+
+    if corrected:
+        carrier = carrier - np.exp(-0.5 * w0 ** 2)  # Corrected carrier wave
+
+    gaussian_window = np.exp(-0.5 * eta ** 2) 
+
+    return normalization * carrier * gaussian_window
+
