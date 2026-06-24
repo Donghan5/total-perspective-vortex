9f6f5c113203915b8f5ce0d6b0d543d02606dc03
donghank <donghank@f6r8s9.paris.42.school>
2026-06-24 16:52:31 +0200

    feature: changed value name

--- src/wavelet_features.py
+++ src/wavelet_features.py
@@ -6,12 +6,12 @@ def mother_morlet(
         corrected: bool = True
 ) -> np.ndarray:
     normalization = np.pi ** (-0.25)
-    carrier = np.exp(1j * w0 * eta)    # Carrier wave
+    complex_sinusoid = np.exp(1j * w0 * eta)    # Carrier wave
 
     if corrected:
-        carrier = carrier - np.exp(-0.5 * w0 ** 2)  # Corrected carrier wave
+        complex_sinusoid = complex_sinusoid - np.exp(-0.5 * w0 ** 2)  # Corrected carrier wave
 
     gaussian_window = np.exp(-0.5 * eta ** 2) 
 
-    return normalization * carrier * gaussian_window
+    return normalization * complex_sinusoid * gaussian_window
 
